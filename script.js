eeba4f405ba5d8d0a763b3a270af74297b0ddc8e
omarfaruk70 <faruk1611281892@gmail.com>
2022-10-01 01:30:27 +0600

    something went wrong! background will not change

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-// this dom project is a random rgb generator by clicking.
+/**
+ * project requirements:
+ * - change the background color by generating random rgb color by clicking a button.
+ */
+ window.onload = () =>{
+    main();
+};
+
+// creat the Background color changes function
+function main(){
+    const root = document.querySelector('body');
+    const btn  = document.getElementById('change-btn');
+    btn.addEventListener('click', function(){
+        let changeBg = generateRGBColor();
+        root.style.backgroundColor =  changeBg; 
+    });
+};
+
+
+// creating a color generator function
+function generateRGBColor(){
+    let red = Math.floor(Math.random()*255);
+    let green = Math.floor(Math.random() * 255);
+    let blue = Math.floor(Math.random() * 255);
+    return `${red}, ${green}, ${blue}`;
+};
